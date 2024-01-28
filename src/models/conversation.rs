6473c279859a7562dd2fd0cad1d8a75698ad2942
Jaures Beinjamin <jauresbeinjamin@gmail.com>
2024-01-28 23:02:42 +0100

    Mise en place une methode pour cree une nouvelle conversation

--- src/models/conversation.rs
+++ src/models/conversation.rs
@@ -8,6 +8,13 @@ pub struct Conversation {
     pub messages: Vec<Message>,
 }
 
+impl Conversation {
+    pub fn new() -> Conversation {
+        Conversation {
+            messages: Vec::new(),
+        }
+    }
+}
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Message {
