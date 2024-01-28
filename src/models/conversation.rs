5d59ee8f0c88196b59e63ab710604b6332eff19f
Jaures Beinjamin <jauresbeinjamin@gmail.com>
2024-01-28 22:59:05 +0100

    Ajout de la bibiotheque

--- src/models/conversation.rs
+++ src/models/conversation.rs
@@ -3,13 +3,13 @@ use serde::{Deserialize, Serialize};
 
 
 
-
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Conversation {
     pub messages: Vec<Message>,
 }
 
 
-
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Message {
     pub user: String,
     pub text: bool,
