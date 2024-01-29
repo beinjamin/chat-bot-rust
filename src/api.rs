0176a98e28cba7fd4c74aba9e359751b29c9f392
Jaures Beinjamin <jauresbeinjamin@gmail.com>
2024-01-29 20:51:42 +0100

    Mise en place du model

--- src/api.rs
+++ src/api.rs
@@ -17,5 +17,55 @@ ServeFnError>{
       
     }).await.unwrap();
     use llm::KnownModel;
+    let character_name="######Assistant";
+    let user_name="######User";
+    let persona="######Persona";
+    let mut history=format!(
+        "{character_name}:Hello - How may I help you today?\n{user_name}:",
+        {user_name}:what is the weather like today?
+        {character_name}:It is sunny and 75 degrees.
+
+    );
+    for message in prompt.messages.into_iter(){
+        let msg = message.text;
+        let curr_line = if message.user{
+            format!("{character_name}:{msg}\n"),    
+        }else{
+            format!("{user_name}:{msg}\n"),
+        };
+
+        history.push_str(&curr_line);
+
+    }
+    let mut res = String::new();
+    let mut rng = rand::thread_rng();
+    let mut buf = String::new();
+    let mut session = model.start_session(Default::default());
+
+session.infer(
+    model.as_ref(),
+    &mut rng,
+    &llm::InferOptions{
+   prompt:format!(
+    "{persona}\n{history}{character_name}:")
+    .as_str()
+    .into(),
+    parameters:Some(&llm::InferenceParameters::defaut()),
+    play_back_previous_tokens:false,
+    maximum_token_count:None,
+
+    },
+    &mut Default::default(),
+    inference_callback(String::from(user_name),&mut buf, &mut res),
+    )
+    .unwrap_or_else(|e|panic!("{}",e));
+Ok(res)
+
+
+
+
+
+
+    Ok(String::from(history)
      
 }
--- src/main.rs
+++ src/main.rs
@@ -45,6 +45,39 @@ async fn favicon(
     ))?)
 }
 
+cfg_if! {
+    if #[cfg(feature = "ssr")] {
+        use llm::models::Llama;
+        use actix_web::*;
+        use std::env;
+        use dotenv::dotenv;
+        fn get_language_model() -> Llama {
+            use std::path::PathBuf;
+            dotenv().ok();
+            let model_path = env::var("MODEL_PATH").expect("MODEL_PATH must be set");
+            let model_parameters = llm::ModelParameters {
+                prefer_mmap: true,
+                context_size: 2048,
+                lora_adapters: None,
+                use_gpu: true,
+                gpu_layers: None,
+                rope_overrides: None,
+                n_gqa: None,
+            };
+
+            llm::load::<Llama>(
+                &PathBuf::from(&model_path),
+                llm::TokenizerSource::Embedded,
+                model_parameters,
+                llm::load_progress_callback_stdout,
+            )
+            .unwrap_or_else(|err| {
+                panic!("Failed to load model from {model_path:?}: {err}")
+            })
+        }
+    }
+}
+
 #[cfg(not(any(feature = "ssr", feature = "csr")))]
 pub fn main() {
     // no client-side main function
