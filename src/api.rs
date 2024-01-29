42b6cf3eaa666bae57bc9cdc56a490f308f5ca5c
Jaures Beinjamin <jauresbeinjamin@gmail.com>
2024-01-29 19:33:24 +0100

    Mise a jour api

--- src/api.rs
+++ src/api.rs
@@ -1,8 +1,21 @@
+use leptos::*;
+use crate::models::conversation::Conversation;
+
+
+
+
+#[server(Converse "/api")]
 pub async fn converse(cx:Scope,prompt:Converstion)->Result<String,
 ServeFnError>{
     use llm::models::Llama;
     use leptos_actix::extract;
     use activix_wb::web::Data;
     use actix_web::dev::ConnectionInfo;
+
+    let model = extract(cx,|data:Data<Llama>,_connection:ConnectionInfo|async{
+        data.into_inner()
+      
+    }).await.unwrap();
+    use llm::KnownModel;
      
 }
